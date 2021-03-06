b4effd8037994445c2d238d1e00c47031d69d168
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-03-06 01:53:58 +0100

    a

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -1,9 +1,9 @@
 cmake_minimum_required (VERSION 3.19.4)
 
-function (ph_name current_dir ret)
-    get_filename_component (${ret} ${current_dir} NAME)
-    string(REPLACE " " "_" ret ${ret})
-endfunction ()
+macro (ph_current_dir_name res)
+    get_filename_component (${res} ${CMAKE_CURRENT_LIST_DIR} NAME CACHE)
+    string(REPLACE " " "_" res ${res})
+endmacro ()
 
 
 
@@ -13,5 +13,51 @@ function (ph_parent_dir current_dir ret)
         cmake_path (GET ${current_dir} PARENT_PATH ${ret})
     endif ()
 endfunction ()
+
+
+function(add_mytest targetName)
+      add_executable(${targetName} ${ARGN})
+      target_link_libraries(${targetName} PRIVATE foobar)
+      add_test(NAME    ${targetName}
+               COMMAND ${targetName}
+      )
+endfunction()
+
+
+
+function (func)
+    # Define the supported set of keywords
+    set(prefix       ARG)
+    set(noValues     ENABLE_NET COOL_STUFF SNOPP)
+    set(singleValues TARGET)
+    set(multiValues  SOURCES IMAGES)
+    # Process the arguments passed in
+    include(CMakeParseArguments)
+    cmake_parse_arguments(${prefix}
+                        "${noValues}"
+                        "${singleValues}"
+                        "${multiValues}"
+                        ${ARGN})
+    # Log details for each supported keyword
+    message("Option summary:")
+    foreach(arg IN LISTS noValues)
+        if(${${prefix}_${arg}})
+            # if (${arg} EQUAL SNOPP)
+            #     message(SNOPP!!!!!!)
+            # endif()
+            message("  ${arg} enabled")
+            # message ("prefix = ${prefix} arg = ${arg}")
+        else()
+            message("  ${arg} disabled")
+        endif()
+    endforeach()
+    foreach(arg IN LISTS singleValues multiValues)
+        # Single argument values will print as a simple string
+        # Multiple argument values will print as a list
+        message("  ${arg} = ${${prefix}_${arg}}")
+        # message ("prefix = ${prefix} arg = ${arg}")
+    endforeach()
+endfunction()
+    
 
 
