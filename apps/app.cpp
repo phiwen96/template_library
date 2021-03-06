d33ea625e84c60a07c6963cb4b1cb183cc3a626a
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-03-06 11:33:03 +0100

    a

--- apps/app.cpp
+++ apps/app.cpp
@@ -1,7 +1,3 @@
-#include <modern/lib.hpp>
-
-#include <fmt/format.h>
-
 #include <iostream>
 #include <vector>
 #include <tuple>
@@ -11,9 +7,8 @@
 int main() {
     std::vector<double> input = {1.2, 2.3, 3.4, 4.5};
 
-    auto [mean, moment] = accumulate_vector(input);
-
-    fmt::print("Mean: {}, Moment: {}\n",  mean, moment);
 
     return 0;
 }
+
+
