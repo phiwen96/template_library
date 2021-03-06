087c1c99a13b90b46e292ede8f627d4c6f5e0afc
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-03-06 14:00:00 +0100

    a

--- apps/app1/app.cpp
+++ apps/app1/app.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
+#include <regex>
 using namespace std;
 
 
 
+
 int main() {
+    
+    
+    
+    string str = "hej kuk$<ph>(x = 2) snopp";
+    
+    regex_replace (str, regex, <#_BidirectionalIterator __last#>, <#const basic_regex<_CharT, _Traits> &__e#>, <#const _CharT *__fmt#>)
+    
     std::vector<double> input = {1.2, 2.3, 3.4, 4.5};
 
     cout << "hello world" << endl;
